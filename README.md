43301dda46e885cda2cf17edf6850c84dead8b84
Max Liu <67022899@qq.com>
2025-07-21 17:43:44 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # html-easy-calc
+
 An easy calculator made by HTML.
+
+I had nothing to tell you, because it's too easy.
+
+用HTML制作的简单计算器。
+
+因为太简单了，没什么好讲的。
+
---
+++ src/js/calc.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+	'use strict';
+	var now = '', func = function todo(id) {
+		var num = id.replace('btn','');
+		if(isNaN(Number(num))) {
+			switch(num) {
+				case 'AC' : now = '';                          break;
+				case 'DEL': now = now.slice(0, now.length - 1); break;
+				case 'MOD': now += '%';                         break;
+				case 'ADD': now += '+';                         break;
+				case 'MIN': now += '-';                         break;
+				case 'TME': now += '*';                         break;
+				case 'DIV': now += '/';                         break;
+				case 'DBZ': now += '00';                        break;
+				case 'DOT': now += '.';                         break;
+				case 'CLC': calc(); return;
+			};
+		} else {
+			now += num;
+		}
+		if(now.length > 7) {
+			err();
+			console.log('Overflow Error');
+		} else {
+			$('.outputText').text(now);
+		}
+	}, calc = function calc() {
+		try {
+			var x = eval(now);
+			$('.outputText').text(x);
+			if(String(x).length > 7) {
+				err();
+				console.log('Overflow Error');
+			}
+			now = '';
+		} catch {
+			err();
+			console.log('Calculate Error');
+		}
+	}, err = function err() {
+		$('.outputText').text('ERROR');
+		now = '';
+	};
+	$('.button').click(function() {
+		func(this.id);
+	});
+});
